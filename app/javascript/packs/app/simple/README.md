2258317da901315a68251d934870758b60bc828b
Tamara Temple <tamouse@gmail.com>
2017-04-29 18:29:13 -0500

    Set up Jest testing on the application
    * configure Jest in package.json

---
+++ app/javascript/packs/app/simple/README.md
@@ -0,0 +1,1 @@
+Just a simple piece of code to apply to Jest testing.
---
+++ app/javascript/packs/app/simple/simple.test.js
@@ -0,0 +1,5 @@
+const simple = require('./simple')
+
+test('adds 1 and 2 and returns 3', () => {
+  expect(simple(1, 2)).toBe(3)
+})
